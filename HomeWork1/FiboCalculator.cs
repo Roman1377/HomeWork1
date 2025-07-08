f1fbf3a2efc512e6d74f9c1638d25921a56e878c
Roman <papa.apy@gmail.comgit config --global user.email papa.apy@gmail.com>
2025-07-08 04:44:09 +0200

    added Ukrainian language recognition

--- HomeWork1/FiboCalculator.cs
+++ HomeWork1/FiboCalculator.cs
@@ -1,39 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork1
 {
-    internal class FiboCalculator
+    public static class FibonacciCalculator
     {
-        public static class FibonacciCalculator
+        // Рекурсивна функція для обчислення n-го числа Фібоначчі
+        public static int GetFibonacci(int n)
         {
-            // Рекурсивна функція для обчислення n-го числа Фібоначчі
-            public static int GetFibonacci(int n)
-            {
-                if (n == 0)
-                    return 0;
-                if (n == 1)
-                    return 1;
-                return GetFibonacci(n - 1) + GetFibonacci(n - 2);
-            }
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
         }
-        class Program
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введіть номер числа Фібоначчі:");
+            if (int.TryParse(Console.ReadLine(), out int n) && n >= 0)
             {
-                Console.WriteLine("Введіть номер числа Фібоначчі:");
-                if (int.TryParse(Console.ReadLine(), out int n) && n >= 0)
-                {
-                    int result = FibonacciCalculator.GetFibonacci(n);
-                    Console.WriteLine($"Число Фібоначчі під номером {n} дорівнює {result}");
-                }
-                else
-                {
-                    Console.WriteLine("Будь ласка, введіть коректне невід'ємне ціле число.");
-                }
+                int result = FibonacciCalculator.GetFibonacci(n);
+                Console.WriteLine($"Число Фібоначчі під номером {n} дорівнює {result}");
+            }
+            else
+            {
+                Console.WriteLine("Будь ласка, введіть коректне невід'ємне ціле число.");
             }
         }
     }
